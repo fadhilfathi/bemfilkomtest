6cc4cfa0cdef2a48b81282c0974078d343921819
fadhilfathi <fadhilfathi.rzm@gmail.com>
2018-03-01 09:52:22 +0800

    update

---
+++ details.php
@@ -0,0 +1,341 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<title>Details</title>
+		<style media="screen">
+		.footer-section {
+				background: #d9ecf2;
+				padding: 50px 0px;
+				color: #022d41;
+		}
+		.footer-section p {
+				font-size: 14px;
+		}
+
+		.footer-section a {
+				color: #ff414d;
+		}
+		@media only screen and (max-width: 555px) {
+			.social-icon li a {
+					width: 40px;
+					height: 40px;
+					line-height: 40px;
+					font-size: 20px;
+			}
+		}
+		.social-icon {
+        list-style: none;
+        padding-left: 0;
+        margin: 0;
+        text-align: center;
+    }
+		.social-icon:before,
+    .social-icon:after {
+        content: " ";
+        display: table;
+    }
+		.social-icon:after {
+				clear: both;
+		}
+		.social-icon li {
+				display: inline-block;
+				margin: 0px 4px;
+		}
+		.social-icon li a {
+				display: block;
+				width: 60px;
+				height: 60px;
+				line-height: 60px;
+				color: #fff;
+				text-align: center;
+				border: 0 none;
+				border-radius: 4px;
+				font-size: 30px;
+				box-shadow: 0px 1px 2px 0px rgba(90, 91, 95, 0.15);
+				transition: all 0.3s ease-in-out;
+		}
+		.facebook {
+				background-color: #3b5998;
+		}
+
+		.twitter {
+				background-color: #1da1f2;
+		}
+
+		.instagram {
+				background-color: #464545;
+		}
+
+		.line {
+				background-color: #02B801;
+		}
+
+		.line img {
+				vertical-align: -32.5%;
+		}
+
+		.dribbble {
+				background-color: #ea4c89;
+		}
+		</style>
+	</head>
+	<body>
+		<nav class="navbar justify-content-between navbar-admin">
+			<a class="navbar-brand"><img src="assets/logo/logo.jpg"><span class="user-def"><span class="margin-def">Detail</span></span></a>
+			<!-- <form class="form-inline">
+				<div class="dropdown">
+					<a class="username" id="dropdown-user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<div class="round-username rounded-circle">
+							<img src="http://cdn2.tstatic.net/suryamalang/foto/bank/images/albert-einstein-arthur-sasse_20160202_131247.jpg">
+						</div>
+						<span class="text-username text-purple">Zain Fikri Hanastyono</span>
+					</a>
+					<div class="dropdown-menu dropdown-u" aria-labelledby="dropdown-user">
+						<a class="dropdown-item" href="#">Logout</a>
+					</div>
+				</div>
+			</form> -->
+		</nav>
+		<br>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<br><p></p>
+					<div class="card card-selection-post">
+						<div class="card-body card-margin-inside-20">
+							<div class="row">
+								<div class="col-md-3">
+									<div class="composite-img-detail rounded">
+										<img src="assets/logo/layer-3@2x.jpg" class="img-fluid img-layer-3">
+									</div>
+								</div>
+								<div class="col-md-8 top-title">
+									<h3 class="card-title card-title-select-post">Ospace</h3>
+									<h5 class="card-subtitle mb-2 text-muted2">Ospace event</h5>
+								</div>
+							</div>
+							<br>
+							<div class="row sizing-below-logo">
+								<div class="col-md-2">
+									<h6 class="color-grey">Details</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat.</h6>
+								</div>
+							</div>
+							<div class="row sizing-below-logo">
+								<div class="col-md-2">
+									<h6 class="color-grey">Position</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2">Manager</h6>
+								</div>
+							</div>
+							<div class="row sizing-below-logo margining-bot-20">
+								<div class="col-md-2">
+									<h6 class="color-grey">Salary</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2">Rp. 200.000</h6>
+								</div>
+							</div>
+							<div class="row sizing-below-logo margining-bot-20">
+								<div class="col-md-2">
+									<h6 class="color-grey">Date</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2">January 2018</h6>
+								</div>
+							</div>
+							<div class="row sizing-below-logo margining-bot-20">
+								<div class="col-md-2">
+									<h6 class="color-grey">Skill</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2">Managing</h6>
+								</div>
+							</div>
+							<div class="row sizing-below-logo margining-bot-20">
+								<div class="col-md-2">
+									<h6 class="color-grey">Location</h6>
+								</div>
+								<div class="col-md-8">
+									<h6 class="text-muted2">Jakarta</h6>
+								</div>
+							</div>
+							<div class="card-link-purple font-lato text-right">
+								<button type="submit" class="btn btn-dark font-lato btn-purple btn-subscribe">See on their site</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- <div class="col-md-4">
+					<p class="font-roboto">You Might Also Like</p>
+							<div class="card card-selection-post">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-md-4">
+											<div class="composite-img rounded-circle">
+												<img src="assets/logo/layer-3.jpg" class="img-fluid img-layer-3">
+											</div>
+										</div>
+										<div class="col-md-7 top-title">
+											<h5 class="card-title card-title-select-post">Ospace</h5>
+											<h6 class="card-subtitle mb-2 text-muted2">Ospace event</h6>
+										</div>
+									</div>
+									<br>
+									<div class="row sizing-below-logo">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Name</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">IFEST</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Location</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Jakarta</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Category</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Paid Event</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Date</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Januari 2018</span>
+										</div>
+									</div>
+									<div class="card-link-purple font-roboto-reg text-right">
+										<a href="#" class="card-link">See details</a>
+									</div>
+								</div>
+							</div>
+					<br>
+							<div class="card card-selection-post">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-md-4">
+											<div class="composite-img rounded-circle">
+												<img src="assets/logo/layer-3.jpg" class="img-fluid img-layer-3">
+											</div>
+										</div>
+										<div class="col-md-7 top-title">
+											<h5 class="card-title card-title-select-post">Ospace</h5>
+											<h6 class="card-subtitle mb-2 text-muted2">Ospace event</h6>
+										</div>
+									</div>
+									<br>
+									<div class="row sizing-below-logo">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Name</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">IFEST</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Location</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Jakarta</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Category</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Paid Event</span>
+										</div>
+									</div>
+									<div class="row sizing-below-logo margining-bot-20">
+										<div class="col-md-4 offset-md-1">
+											<p class="color-grey">Date</p>
+										</div>
+										<div class="col-md-7">
+											<span class="text-muted2">Januari 2018</span>
+										</div>
+									</div>
+									<div class="card-link-purple font-roboto-reg text-right">
+										<a href="#" class="card-link">See details</a>
+									</div>
+								</div>
+							</div>
+				</div> -->
+			</div>
+		</div>
+		<br><br>
+		<footer class="footer-section">
+				<div class="margin-bottom-30">
+						<ul class="social-icon" id="contact">
+								<li><a href="https://www.facebook.com/bemui2017" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/bemui_official" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.instagram.com/bemui_official" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a></li>
+								<li><a href="mailto:someone@example.com" target="_blank" class="dribbble"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+								<li>
+										<a href="https://line.me/R/ti/p/%40opl2452d" target="_blank" class="line"><img src="images/LINE_Icon.png"></a>
+								</li>
+						</ul>
+				</div>
+				<div class="container">
+						<div class="row">
+
+								<div class="col-md-4 text-center">
+										<p>© Copyright 2018 BEM FILKOM</p>
+								</div>
+								<!-- <div class="col-md-4 uipasta-credit text-right">
+								<p>Design By <a href="http://www.uipasta.com" target="_blank" title="UiPasta">UiPasta</a></p>
+								</div> -->
+						</div>
+				</div>
+		</footer>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+		<script type="text/javascript">
+			$('#date').bootstrapMaterialDatePicker({ weekStart : 0, time: false });
+		</script>
+		<script>
+		function initMap() {
+		var uluru = {lat: -7.9536916, lng: 112.6146732};
+		var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: uluru
+		});
+		var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+		});
+		}
+		</script>
+		<script async defer
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5M2b8xx5D28aonuIs1RbVb4TcRFUT7Bg&callback=initMap">
+		</script>
+	</body>
+</html>
